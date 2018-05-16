a944f81cc517534673025fb73858ff1c97cd639f
Ashleigh Bunkofske <bunkas01@luther.edu>
2018-05-15 21:23:36 -0500

    Created python script for sampling manageable subset of data while retaining integrity. The full dataset was causing regular kernel crashes during the sampling process, even when using Dask to parallelise the workload.

---
+++ downsampler.py
@@ -0,0 +1,16 @@
+"""Script to downsample R6:S data for final project."""
+
+import re
+
+
+sampler = re.compile("(201702)12.+?PC.*")
+subset = []
+with open("datadump_S5.csv", encoding="windows-1252") as file:
+    for line in file:
+        if 'dateid' in line:
+            subset.append(line.split(";"))
+        if sampler.match(line):
+            subset.append(line.split(";"))
+
+with open("downsampled_datadump.csv", mode="w") as output_file:
+    output_file.write("\n".join([",".join(line) for line in subset]))
